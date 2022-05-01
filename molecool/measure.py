199b47163032c423f649890ef3ced57e7574b2b4
rdguha <rdguha@lbl.gov>
2022-05-01 15:26:46 -0700

    organize molecool into molecules and subpackage

---
+++ molecool/measure.py
@@ -0,0 +1,60 @@
+"""
+This module is for functions that perform measurements.
+"""
+import numpy as np
+def calculate_distance(rA, rB):
+    """Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+
+    dist_vec = rA - rB
+    distance = np.linalg.norm(dist_vec)
+
+    return distance
+
+def calculate_angle(rA, rB, rC, degrees=False):
+    """Calculate the angle between three points/sites.
+
+    Parameters
+    ----------
+    rA, rB, rC : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    theta : float
+        The angle between the three points.
+
+    Examples
+    --------
+    >>> rA = np.array([0, 0, 0])
+    >>> rB = np.array([0, 1, 0])
+    >>> rC = np.array([1, 1, 0])
+    >>> calculate_angle(rA, rB, rC)
+    1.5708
+    """
+
+    AB = rB - rA
+    BC = rB - rC
+    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+
+    if degrees:
+        return np.degrees(theta)
+    else:
+        return theta
