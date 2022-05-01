868de2024e090f7f13ec736942e17bf018481d0b
rdguha <rdguha@lbl.gov>
2022-05-01 14:54:03 -0700

    added docstring to open_pdb

--- molecool/functions.py
+++ molecool/functions.py
@@ -49,9 +49,28 @@ def zen(with_attribution=True):
 
     return quote
 
-def open_pdb(f_loc):
+def open_pdb(file_location):
+    """Open and read coordinates and atom symbols from a pdb file.
+
+    The pdb file must specify the atom elements in the last column, and follow
+    the conventions outlined in the PDB format specification.
+
+    Parameters
+    ----------
+    file_location : str
+        The location of the pdb file to read in.
+
+    Returns
+    -------
+    coords : np.ndarray
+        The coordinates of the pdb file.
+    symbols : list
+        The atomic symbols of the pdb file.
+
+    """
+    
     import numpy as np
-    with open(f_loc) as f:
+    with open(file_location) as f:
         data = f.readlines()
     c = []
     sym = []
